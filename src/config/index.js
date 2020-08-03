ec7f33e3a276c5b10cbc5e9d4b7ef3a2f9213a82
Yaron Nehushtan <ne.yaron@gmail.com>
2020-08-03 20:43:16 +0300

    react production

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,11 @@
 import development from './development';
+import production from './production';
 
-export default development
+let env = development;
+
+if(process.env.NODE_ENV === 'production') {
+    env = production;
+}
+
+
+export default env
